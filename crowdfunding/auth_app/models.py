6dd12b97a758ba4709b0667854dc090d88794b7c
Ahmed Abdelnasser <ahmed.abdelnasser7777777@gmail.com>
2023-02-12 20:52:52 +0200

    template , models , register , login

--- crowdfunding/auth_app/models.py
+++ crowdfunding/auth_app/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
 class RegisterUser(models.Model):
-    user_id=models.AutoField(primary_key=True)
+    user_id = models.AutoField(primary_key=True)
     first_name=models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    user_email=models.CharField(max_length=70)
+    user_email = models.CharField(max_length=70)
     user_password = models.CharField(max_length=30)
-    user_mobile = models.IntegerField
-    user_image = models.ImageField
+    user_mobile = models.CharField(max_length=11)
+    user_image = models.ImageField(max_length=255)
 
-class
+
+#models.SlugField(max_length=255)
--- crowdfunding/auth_app/views.py
+++ crowdfunding/auth_app/views.py
@@ -1,26 +1,28 @@
-import random
 
-from django.shortcuts import render
+from .models import *
 from rest_framework.views import APIView
 from twilio.rest import Client
 from django.http import JsonResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http.response import HttpResponse
+from django.http.response import HttpResponseRedirect
 # Create your views here.
-class sendOTP(APIView):
-    def post(self, r):
-        account_sid = "AA"
-        auth_token = "ASA"
-        number = r.data['number']
-        client = Client(account_sid, auth_token)
-        otp = generateOTP()
-        body = "Your OTP is"+ str(otp)
-        message = client.messages.create(from_= "Abdelnasser ", body = body, to = number )
-        if message.sid:
-            print("send Successfull")
-            return JsonResponse({"success": True})
-        else:
-            print("Send fail")
-            return JsonResponse({"success": False})
-def generateOTP():
-    return random.randrange(1000000,9999999)
 
+def testTemp(r):
+    return render(r, 'index.html')
+
+def register(r):
+    if(r.method=='POST'):
+         RegisterUser.objects.create(
+             first_name=r.POST['first_name'],
+             last_name=r.POST['last_name'],
+             user_email=r.POST['user_email'],
+             user_password=r.POST['user_password'],
+             user_mobile=r.POST['user_mobile'],
+             user_image=r.POST['user_image'],
+         )
+         return render(r,'index.html')
+    else:
+        return render(r, 'register.html')
 
