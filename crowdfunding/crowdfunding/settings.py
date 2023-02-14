e3dd56396f4480742e96a26563c8fa3a5b82cd1e
mahmoud hassan <abosamour990@gmail.com>
2023-02-14 13:39:35 +0200

    fix bugs

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'crowdfunding',
         'USER':'postgres',
-        'PASSWORD':'mahm01142',
+        'PASSWORD':'123245',
         'HOST':'127.0.0.1',
         'PORT':5432
     }
--- crowdfunding/posts_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PostsAppConfig(AppConfig):
-    default_auto_field = 'django.db.modals.BigAutoField'
-    name = 'posts_app'
