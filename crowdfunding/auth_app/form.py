b9244249f68bc229772d74fe080e8533a6134c54
Ahmed Abdelnasser <ahmed.abdelnasser7777777@gmail.com>
2023-02-18 19:18:51 +0200

    fix registeration errors 'i wish'

--- crowdfunding/auth_app/form.py
+++ crowdfunding/auth_app/form.py
@@ -1,30 +1,30 @@
 from django import forms
 from django.core.validators import RegexValidator
+from django.contrib.auth.hashers import make_password
 from .models import RegisterUser
-import sys
 
-sys.setrecursionlimit(1500)
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     confirm_password = forms.CharField(widget=forms.PasswordInput)
-    mobile_phone = forms.CharField(validators=
-          [RegexValidator(regex='^(01)[0125][0-9]{8}$', message='wrong number')])
+    mobile_phone = forms.CharField(validators=[
+        RegexValidator(regex='^(01)[0125][0-9]{8}$', message='wrong number')
+    ])
 
     class Meta:
         model = RegisterUser
-        fields = ['first_name', 'last_name', 'user_email','user_image']
+        fields = ['first_name', 'last_name', 'user_email', 'user_image']
 
     def clean(self):
-        password = self.cleaned_data.get('user_password')
-        confirm_password = self.cleaned_data.get('confirm_password')
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
         if password and confirm_password and password != confirm_password:
             raise forms.ValidationError("passwords are not matched !!!")
-
-
+        return cleaned_data
 
     def save(self, commit=True):
-        user = self.save(commit=True)
-        user.user_password(self.cleaned_data['user_password'])
+        user = super().save(commit=False)
+        user.user_password = make_password(self.cleaned_data['password'])
         user.is_active = False
         if commit:
             user.save()
--- crowdfunding/auth_app/models.py
+++ crowdfunding/auth_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.conf.urls.static import static
 from django.core.validators import RegexValidator
+from django.contrib.auth.hashers import make_password
 
 
 
@@ -9,13 +10,11 @@ from django.core.validators import RegexValidator
 
 class RegisterUser(models.Model):
     user_id = models.AutoField(primary_key=True)
-    first_name=models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    first_name=models.CharField(max_length=29)
+    last_name = models.CharField(max_length=28)
     user_email = models.EmailField(max_length=70)
-    user_password = models.CharField(max_length=30)
+    user_password = models.CharField(max_length=150)
     user_mobile = models.CharField(max_length=11, validators=[
         RegexValidator(regex='^(01)[0125][0-9]{8}$', message='wrong number')])
     user_image = models.ImageField(upload_to ='image/', null=True, blank=True )
 
-
-#models.SlugField(max_length=255)
